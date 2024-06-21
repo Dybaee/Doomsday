20cf8ce7bdb5d79e6d888d4c347010191a171696
NXTR15 <abielheroes5@gmail.com>
2024-06-21 14:51:42 +0700

    VFX slash dan toon shader di basic enemies

---
+++ GKM UI FIX/Assets/Script/VFXSlash.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class VFXSlash : MonoBehaviour
+{
+    [SerializeField] private VisualEffect Slash1;
+    [SerializeField] private VisualEffect Slash2;
+    [SerializeField] private VisualEffect Slash3;
+    public void Attack1()
+    {
+        Slash1.Play();
+    }
+
+    public void Attack2()
+    {
+        Slash2.Play();
+    }
+
+    public void Attack3()
+    {
+        Slash3.Play();
+    }
+}
